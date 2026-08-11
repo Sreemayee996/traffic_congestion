845158fe6bd280220570f04c1406a99558ca25a0
Sreemayee996 <smayeep@gmail.com>
2026-08-11 22:18:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -209,5 +209,5 @@ def bottlenecks_batch():
         return _handle_not_ready_error(e)
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     #app.run(debug=True, host="0.0.0.0", port=5000)
